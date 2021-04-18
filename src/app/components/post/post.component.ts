6cfab47ee76432b033c8ee048bdaaa23481dc35d
kunal <kunalshelke123@gmail.com>
2021-04-18 06:54:11 +0530

    saved

---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import {faThumbsUp,faThumbsDown,faShareSquare} from "@fortawesome/free-regular-svg-icons";
+import { AuthService } from 'src/app/serivices/auth.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit, OnChanges {
+
+  faThumbsUp=faThumbsUp;
+  faThumbsDown=faThumbsDown;
+  faShareSquare=faShareSquare;
+
+
+  uid=null;
+  upvote=0;
+  downvote=0;
+
+
+  @Input() post;
+  constructor(private db:AngularFireDatabase,
+    private auth:AuthService) { 
+      this.auth.getUser().subscribe((user)=>{
+        this.uid=user?.uid;
+      })
+    }
+
+  ngOnInit(): void {
+  }
+
+  //TODO:bug in updating the changes.
+  ngOnChanges():void{
+    if(this.post.vote){
+      Object.values(this.post.vote).map((val:any)=>{
+        if(val.upvote){
+          this.upvote +=1;
+        }
+        if(val.downvote)
+        {
+          this.downvote +=1;
+        }
+      });
+    }
+  }
+
+  upvotePost(){
+    console.log("UPVOTING");
+    this.db.object(`/posts/${this.post.id}/vote/${this.uid}`).set({
+      upvote:1,
+    });
+  }
+
+
+  downvotePost(){
+    console.log("DOWNVOTING");
+    this.db.object(`/posts/${this.post.id}/vote/${this.uid}`).set({
+      downvote:1,
+    });
+  }
+
+
+  getInstaUrl(){
+    return `https://instagram.com/${this.post.instaId}`
+  }
+
+
+  
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBoJiFr6mT57ySE9A9U9XgAWcsGMzNb9js",
+    authDomain: "angula30-7d7fb.firebaseapp.com",
+    databaseURL: "https://angula30-7d7fb-default-rtdb.firebaseio.com",
+    projectId: "angula30-7d7fb",
+    storageBucket: "angula30-7d7fb.appspot.com",
+    messagingSenderId: "247620765441",
+    appId: "1:247620765441:web:5af18794302ee28ff4bd00"
+  },
 };
 
 /*
